dc63d4b7296297620d17fdd8f734932f8949b554
DejanDopudj <dejandopudj01@gmail.com>
2023-12-25 01:59:23 +0100

    Implement everything

---
+++ sem/src/main.rs
@@ -0,0 +1,123 @@
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum Value {
+    Integer(i32),
+    Double(f64),
+    String(String),
+}
+
+struct DynamicThreadPoolServer {
+    host: String,
+    port: u16,
+    dictionary: Arc<RwLock<HashMap<String, Value>>>,
+}
+
+impl DynamicThreadPoolServer {
+    fn new(host: &str, port: u16) -> Self {
+        DynamicThreadPoolServer {
+            host: host.to_string(),
+            port,
+            dictionary: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+
+    async fn start_server(&self) {
+        let listener = TcpListener::bind(format!("{}:{}", self.host, self.port)).await.unwrap();
+        println!("Server listening on {}:{}", self.host, self.port);
+        while let Ok((stream, _)) = listener.accept().await {
+            let dictionary = Arc::clone(&self.dictionary);
+            tokio::spawn(async move {
+                Self::handle_client_with_dictionary(stream, dictionary).await;
+            });
+        }
+    }
+
+    async fn handle_client_with_dictionary(mut stream: TcpStream, dictionary: Arc<RwLock<HashMap<String, Value>>>) {
+        let mut buffer = [0; 1024];
+        if let Ok(_) = stream.read(&mut buffer).await {
+            let request = String::from_utf8_lossy(&buffer[..]);
+
+            let (response_body, status_line) = if request.starts_with("PUT") {
+                if let Some(body) = request.split("\r\n\r\n").nth(1) {
+                    let trimmed_body = body.trim();
+                    let res_option = trimmed_body.split('}').next();
+                    if let Some(res) = res_option {  
+                        let mut owned_res = res.to_owned();
+                        owned_res.push('}');
+                        let json: serde_json::Value = serde_json::from_str(&owned_res)
+                            .expect("JSON was not well-formatted");
+                        // println!("{}", json);
+                        // Extract key and value from JSON
+
+                        if let (Some(key), Some(value)) = (json.get("key"), json.get("value")) {
+                            let key = key.as_str().unwrap().to_string();
+                            let value = match value {
+                                serde_json::Value::String(s) => Value::String(s.to_string()),
+                                serde_json::Value::Number(n) if n.is_i64() => Value::Integer(n.as_i64().unwrap() as i32),
+                                serde_json::Value::Number(n) => Value::Double(n.as_f64().unwrap()),
+                                _ => Value::String("NULL".to_string()),
+                            };
+                
+                            Self::put_with_dictionary(&dictionary, key, value);
+                            ("PUT successful".to_string(), "HTTP/1.1 200 OK")
+                        } else {
+                            ("Invalid JSON".to_string(), "HTTP/1.1 400 Bad Request")
+                        }
+                    } else {
+                        ("Invalid JSON".to_string(), "HTTP/1.1 400 Bad Request")
+                    }
+                } else {
+                    ("Invalid request body".to_string(), "HTTP/1.1 400 Bad Request")
+                }
+            }
+            else if request.starts_with("GET") {
+                if let Some(key) = request.split("?").nth(1).unwrap_or("").split(" ").next() {
+                    let value = Self::get_with_dictionary(&dictionary, key.split("=").nth(1).unwrap_or(""));
+                    (format!("GET result: {:?}", value), "HTTP/1.1 200 OK")
+                } else {
+                    ("Invalid GET request".to_string(), "HTTP/1.1 400 Bad Request")
+                }
+            } else {
+                ("Invalid request".to_string(), "HTTP/1.1 400 Bad Request")
+            };
+
+            // Self::print_dict_info_with_dictionary(&dictionary);
+            let response_headers = "Content-Type: text/plain\r\n";
+            let response = format!("{}\r\n{}\r\n\r\n{}", status_line, response_headers, response_body);
+
+            if let Err(_) = stream.write(response.as_bytes()).await {
+                eprintln!("Error writing response to stream");
+            }
+        }
+    }
+
+    fn put_with_dictionary(dictionary: &Arc<RwLock<HashMap<String, Value>>>, key: String, value: Value) {
+        let mut dictionary = dictionary.write().unwrap();
+        dictionary.insert(key, value);
+    }
+
+    fn get_with_dictionary(dictionary: &Arc<RwLock<HashMap<String, Value>>>, key: &str) -> Value {
+        let dictionary = dictionary.read().unwrap();
+        dictionary.get(key).cloned().unwrap_or(Value::String("NULL".to_string()))
+    }
+
+    fn print_dict_info_with_dictionary(dictionary: &Arc<RwLock<HashMap<String, Value>>>) {
+        for (key, value) in dictionary.read().unwrap().iter() {
+            println!("Key: {}", key);
+            println!("Value: {:?}", value);
+            println!("Type of Value: {:?}", std::mem::discriminant(value));
+            println!("------");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let server = DynamicThreadPoolServer::new("127.0.0.1", 8082);
+    server.start_server().await;
+}
---
+++ server.py
@@ -0,0 +1,89 @@
+import socket
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import json
+import time
+
+class DynamicThreadPoolServer:
+    def __init__(self, host, port, initial_threads=5, max_threads=20, backlog_threshold=10):
+        self.host = host
+        self.port = port
+        self.initial_threads = initial_threads
+        self.max_threads = max_threads
+        self.backlog_threshold = backlog_threshold
+        self.dictionary = {}
+        self.lock = threading.Lock()
+        self.executor = None
+        self.i = 0
+        self.start_time = None
+    def start_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.host, self.port))
+        server_socket.listen(20000)  # Increased backlog to 100
+        print("Server listening on {}:{}".format(self.host, self.port))
+
+        self.executor = ThreadPoolExecutor(max_workers=self.initial_threads)
+        while True:
+            client_socket, address = server_socket.accept()
+            self.executor.submit(self.handle_client, client_socket)
+
+    def print_dict_info(self):
+        for key, value in self.dictionary.items():
+            print("Key: {}".format(key))
+            print("Value: {}".format(value))
+            print("Type of Value: {}".format(type(value)))
+            print("------")
+
+    def handle_client(self, client_socket):
+        if self.i == 0:
+            self.start_time = time.time()
+        request = client_socket.recv(1024).decode('utf-8')
+        if request.startswith("PUT"):
+            body = request.split("\r\n\r\n")[1]
+            data = json.loads(body)
+            #print(data.keys())
+            key = data["key"]
+            value = data["value"]
+
+            self.put(key, value)
+
+            response_body = "PUT successful"
+            status_line = "HTTP/1.1 200 OK"
+
+        elif request.startswith("GET"):
+            key = request.split("?")[1].split(" ")[0].split("=")[1]
+            value = self.get(key)
+            response_body = "GET result: {}".format(value)
+            status_line = "HTTP/1.1 200 OK"
+
+        else:
+            response_body = "Invalid request"
+            status_line = "HTTP/1.1 400 Bad Request"
+        #self.print_dict_info()
+        response_headers = "Content-Type: text/plain\r\n"
+        response = "{}\r\n{}\r\n\r\n{}".format(status_line, response_headers, response_body)
+
+        self.i+=1
+        # print(self.i)
+        if self.i >= 10000:
+            total_time = time.time() - self.start_time
+            print(total_time)
+        client_socket.send(response.encode('utf-8'))
+        client_socket.close()
+
+
+
+    def put(self, key, value):
+        with self.lock:
+            # print("Putting", key)
+            self.dictionary[key] = value
+
+    def get(self, key):
+        with self.lock:
+            # print("Getting", key)
+            return self.dictionary.get(key, "NULL")
+
+# Usage
+if __name__ == "__main__":
+    server = DynamicThreadPoolServer("127.0.0.1", 8081, initial_threads=5, max_threads=12, backlog_threshold=10)
+    server.start_server()
